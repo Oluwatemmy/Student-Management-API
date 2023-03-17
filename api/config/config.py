8ed05c2d7684ef106920b81f39faf59e523afd02
Ajayi Oluwaseyi <oluwaseyitemitope456@gmail.com>
2023-03-17 19:09:31 +0100

    Added heroku files

--- api/config/config.py
+++ api/config/config.py
@@ -4,6 +4,12 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
+default_uri = "postgres://{}:{}@{}/{}".format('postgres', 'password', 'localhost:5432', 'student_api_db')
+
+uri = os.getenv('DATABASE_URL', default_uri) # or other relevant config var
+if uri.startswith('postgres://'):
+    uri = uri.replace('postgres://', 'postgresql://', 1)
+
 class Config:
     SECRET_KEY = config('SECRET_KEY', 'secret')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
@@ -23,7 +29,9 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = uri
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = config('DEBUG', False, cast=bool)
 
 config_dict = {
     'dev': DevConfig,
